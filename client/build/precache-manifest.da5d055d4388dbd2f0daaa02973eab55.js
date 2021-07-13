856e1b1239f80ca9b7b582897f69bd2e3bbca9ef
dvTinh99 <tinh5969@gmail.com>
2021-07-13 11:59:42 +0700

    first commit

---
+++ client/build/precache-manifest.da5d055d4388dbd2f0daaa02973eab55.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "795d9e900ada547eb1299b82945f822d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d1b1b023577956d7312",
+    "url": "/static/css/2.190caba9.chunk.css"
+  },
+  {
+    "revision": "28ed7c1f6dd973b1033e",
+    "url": "/static/css/3.d06eeaec.chunk.css"
+  },
+  {
+    "revision": "d270bd95ac063f03a62a",
+    "url": "/static/css/main.faeaf4b4.chunk.css"
+  },
+  {
+    "revision": "1d1b1b023577956d7312",
+    "url": "/static/js/2.17a7246f.chunk.js"
+  },
+  {
+    "revision": "148709ddc638d9a155f2e9c428088f0a",
+    "url": "/static/js/2.17a7246f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28ed7c1f6dd973b1033e",
+    "url": "/static/js/3.665ad664.chunk.js"
+  },
+  {
+    "revision": "d270bd95ac063f03a62a",
+    "url": "/static/js/main.1b8f5a42.chunk.js"
+  },
+  {
+    "revision": "383de4a487644967cbae",
+    "url": "/static/js/runtime-main.bf23bad8.js"
+  },
+  {
+    "revision": "feb48833bac136f91aab9b27583fb981",
+    "url": "/static/media/camera-stop.feb48833.svg"
+  },
+  {
+    "revision": "ff36203ce9be4c3b857c6e7648966b07",
+    "url": "/static/media/camera.ff36203c.svg"
+  },
+  {
+    "revision": "00ea149ed9cde2a8afc40b10b54c2844",
+    "url": "/static/media/cuckoo-logo.00ea149e.svg"
+  },
+  {
+    "revision": "7722d32d9ff3e121881324b81968f255",
+    "url": "/static/media/fullscreen.7722d32d.svg"
+  },
+  {
+    "revision": "d3ae4842456ead735aecd20d34c2d80c",
+    "url": "/static/media/hang-up.d3ae4842.svg"
+  },
+  {
+    "revision": "bbec867b3bc32a35a52f068fe692f735",
+    "url": "/static/media/microphone-stop.bbec867b.svg"
+  },
+  {
+    "revision": "f375b0a7d2bf1c4703b4a98b5beeb1b8",
+    "url": "/static/media/microphone.f375b0a7.svg"
+  },
+  {
+    "revision": "612b180a63a0a715724db3a0be971688",
+    "url": "/static/media/minimize.612b180a.svg"
+  },
+  {
+    "revision": "0be3c651e1532de63056a20e2f341bde",
+    "url": "/static/media/ringtone.0be3c651.mp3"
+  },
+  {
+    "revision": "269beb7bd8d4931225aadb7103aa2be9",
+    "url": "/static/media/share.269beb7b.svg"
+  }
+]);
